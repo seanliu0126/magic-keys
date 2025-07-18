5a17989403acecf729b89a7742295856f8c11054
Sean H. Liu <seanliu@Seans-Laptop.local>
2025-07-18 13:39:08 +1000

    Your update message

--- script.js
+++ script.js
@@ -32,3 +32,25 @@ window.addEventListener("load", function () {
     requestAnimationFrame(scroll);
   });
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("modal");
+  const openBtn = document.getElementById("openModal");
+  const closeBtn = document.getElementById("closeModal");
+
+  openBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+});
